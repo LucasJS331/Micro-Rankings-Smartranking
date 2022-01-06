713ca06b3fab248890cc3cfba8155e1d4c5ca164
LucasJS331 <69942512+LucasJS331@users.noreply.github.com>
2022-01-06 19:27:25 -0300

    update configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { RankingsModule } from './rankings/rankings.module';
 import { MongooseModule } from '@nestjs/mongoose'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule,ConfigService } from '@nestjs/config'
 import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
 
+const config_service = new ConfigService();
 
 @Module({
   imports: [
     RankingsModule,
     ConfigModule.forRoot({isGlobal: true}),
-    MongooseModule.forRoot('mongodb+srv://admin_sr:kdWcpu1jWvUSlU6Y@clustermogodb-79l5n.mongodb.net/srranking?retryWrites=true&w=majority',
+    MongooseModule.forRoot(config_service.get("MONGO_URI"),
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
     ProxyRMQModule
   ],
